3dad3946158227108e2f888fd80d4ebea3d78ca9
SUSHANT KUMAR <sushantk.ai031@gmail.com>
2026-06-27 11:07:20 +0530

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.models.ai_data import AIData
-from app.models.candidate import Candidate
-from app.models.job import Job
-from app.models.match import Match
-from app.models.resume import Resume
-from app.models.user import User
-
